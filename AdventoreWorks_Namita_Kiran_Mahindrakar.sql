c550fe2bf77145b4ec97f2ce4a9547b71234e50a
Namita Mahindrakar <namitamahindrakar@gmail.com>
2024-02-11 23:59:43 -0800

    Add files via upload

---
+++ AdventoreWorks_Namita_Kiran_Mahindrakar.sql
@@ -0,0 +1,42 @@
+
+--Problem 1
+select u.Name, t.inventorysum, sum(ASS.OrderQty) as ordersum
+from [AdventureWorks2019].[Sales].[SalesOrderDetail] as ASS, (select AP.ProductID, AP.Name from [AdventureWorks2019].[Production].[Product] as AP) as u, 
+(select APP.ProductID, sum(APP.Quantity) as inventorysum from [AdventureWorks2019].[Production].[ProductInventory] as APP group by APP.ProductID) as t
+where u.ProductID = ASS.ProductID and ASS.ProductID = t.ProductID
+group by u.Name, t.inventorysum
+order by ordersum DESC
+
+--Problem 2
+select u.Name, t.inventorysum, sum(ASS.OrderQty) as ordersum into production_product_details
+from [AdventureWorks2019].[Sales].[SalesOrderDetail] as ASS, (select distinct AP.ProductID, AP.Name from [AdventureWorks2019].[Production].[Product] as AP) as u, 
+(select APP.ProductID, sum(APP.Quantity) as inventorysum from [AdventureWorks2019].[Production].[ProductInventory] as APP group by APP.ProductID) as t
+where u.ProductID = ASS.ProductID and ASS.ProductID = t.ProductID
+group by u.Name, t.inventorysum
+order by ordersum DESC
+
+Select * from production_product_details
+
+--Problem 3
+select * from production_product
+where inventorysum < 100
+
+--Problem 4
+select name from Production.Product
+where ProductNumber like 'FR%'
+
+select ProductID,
+name,
+SUBSTRING(name,1,3) as Prefix, 
+SUBSTRING(name, PATINDEX('% %',name)+1 , PATINDEX('%-%',name)-PATINDEX('% %', name)-1) as Type, 
+CASE 
+	WHEN name like '%W%' then 'F'
+	ELSE 'M'
+END as Gender, 
+SUBSTRING(name,len(name)-1,3) as Size, 
+SUBSTRING(name, PATINDEX('%-%',name)+1 , PATINDEX('%,%',name)- PATINDEX('%-%', name)-1) as Color 
+INTO Product_Number 
+from production.product 
+where ProductNumber like 'FR%'; 
+
+select * from Product_Number;
---
+++ HWSQL02 - AdventureWorks_Namita_Kiran_Mahindrakar.sql
@@ -0,0 +1,46 @@
+
+--1
+select APP.FirstName, APP.LastName, AHE.HireDate, dept.Name
+from [AdventureWorks2019].[HumanResources].[Employee] as AHE, [AdventureWorks2019].[Person].[Person] as APP, 
+(select max(HireDate) as highest from [AdventureWorks2019].[HumanResources].[Employee] as AHE) as t,
+[AdventureWorks2019].[HumanResources].[Department] as dept, [AdventureWorks2019].[HumanResources].[EmployeeDepartmentHistory] as empdept 
+where DATEDIFF(DAY,AHE.HireDate, t.highest) <=365 and AHE.BusinessEntityID =APP.BusinessEntityID and APP.BusinessEntityID = empdept.BusinessEntityID 
+and empdept.DepartmentID = dept.DepartmentID
+
+--2
+
+select FirstName, Lastname, dept.Name,
+Case when emppay.ratechangedate >= (Select DATEADD(DAY,-365, max(HireDate)) from [AdventureWorks2019].[HumanResources].[Employee])
+then 'Raise' else 'No Raise'
+end as Hike
+from Person.Person as person,  HumanResources.Employee as emp, HumanResources.Department as dept,
+HumanResources.EmployeeDepartmentHistory as depthist, HumanResources.EmployeePayHistory as emppay
+where person.BusinessEntityID = emp.BusinessEntityID and dept.DepartmentID = depthist.DepartmentID and
+depthist.BusinessEntityID = emp.BusinessEntityID
+and emppay.BusinessEntityID = emp.BusinessEntityID
+
+
+--3
+
+select person.FirstName, person.LastName, count(salesheader.SalesOrderID) as No_Of_Orders, sum(salesheader.TotalDue) as Total_Of_Orders
+from [AdventureWorks2019].[Person].[Person] as person,[AdventureWorks2019].[Sales].[SalesOrderHeader] as salesheader, [AdventureWorks2019].[Sales].[Customer] as cust
+where person.BusinessEntityID = cust.PersonID and salesheader.CustomerID = cust.CustomerID
+group by person.FirstName, person.LastName
+order by No_Of_Orders,Total_Of_Orders
+
+--4
+
+SELECT * FROM Sales.CreditCard;
+
+CREATE USER CreditCardMask WITHOUT LOGIN; 
+GRANT SELECT ON Sales.CreditCard  TO CreditCardMask;
+
+DROP INDEX AK_CreditCard_CardNumber ON Sales.CreditCard;
+
+ALTER TABLE Sales.CreditCard  
+ALTER COLUMN CardNumber VARCHAR(20) MASKED WITH (FUNCTION = 'partial(0,"****",2)');
+
+EXECUTE AS USER = 'CreditCardMask'
+SELECT * FROM Sales.CreditCard  
+REVERT
+GO
